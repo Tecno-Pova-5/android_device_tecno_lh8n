39bdf9b580fe99397cf130aad931b51e52b2f47b
Sushrut1101 <guptasushrut@gmail.com>
2023-10-19 16:11:07 +0530

    everpal: Configure `batterysecret` for Fast Charging
    * Also remove seclabel option for it in init.batterysecret.rc
    
    Change-Id: Ia007d7deb95d2efaefb23b4d31d1908dc6e0c1fb
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -111,6 +111,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.mt6833 \
     fstab.mt6833.ramdisk \
+    init.batterysecret.rc \
     init.connectivity.rc \
     init.modem.rc \
     init.mt6833.power.rc \
