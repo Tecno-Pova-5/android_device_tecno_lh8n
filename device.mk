497ccdf1ce70541a800540200ceaa83167563975
Sushrut1101 <guptasushrut@gmail.com>
2025-07-08 02:31:33 +0530

    everpal: Build MediaTek Health AIDL
    Change-Id: I4343e1c5b4d37c67d4a3dd264a339235fc8b00c6
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -52,6 +52,12 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health-service.mediatek \
+    android.hardware.health-service.mediatek-recovery \
+    charger_res_images_vendor
+
 # Light
 PRODUCT_PACKAGES += \
     android.hardware.light-service.lineage
