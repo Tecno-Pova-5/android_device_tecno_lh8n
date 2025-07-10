2ee6bc35176d3a699f6cbd89e5e9e8b3ae4815d2
Sushrut1101 <guptasushrut@gmail.com>
2025-07-11 02:53:54 +0530

    everpal: Configure Media stack
    Change-Id: Ibac4a3fd671d2f71d7e05e08607f566661f313d9
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -127,6 +127,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.lineage
 
+# Media
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/media,$(TARGET_COPY_OUT_VENDOR)/etc)
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
--- extract-files.py
+++ extract-files.py
@@ -64,6 +64,10 @@ def blob_fixup_return_1(
             break
 
 blob_fixups: blob_fixups_user_type = {
+    'vendor/etc/init/android.hardware.media.c2@1.2-mediatek-64b.rc': blob_fixup()
+        .regex_replace('mediatek', 'mediatek-64b'),
+    'vendor/etc/vintf/manifest/manifest_media_c2_V1_2_default.xml': blob_fixup()
+        .regex_replace('1.1', '1.2'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
